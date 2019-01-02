b80a8f96aa9ff18286334df213d20241bbab4449
Philon <id@philon.cn>
2019-01-02 23:17:13 +0800

    😫 I will be back

--- algorithm/019-remove_nth_node_from_end_of_list/c/solution.h
+++ algorithm/019-remove_nth_node_from_end_of_list/c/solution.h
@@ -1,1 +1,32 @@
-// todo: algorithm implementation
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+  int len = n + 3;
+  struct ListNode** cache = (struct ListNode**)calloc(len, sizeof(void*));
+  struct ListNode* node = head;
+  int i = 0;
+
+  while (node) {
+    cache[i] = node;
+    node = node->next;
+    i = (i <= n) ? i+1 : 1;
+  }
+
+  cache[0] = cache[n];
+  cache[len-1] = cache[1];
+
+  if (cache[i]) {
+    // 正常移除
+    cache[i]->next = n == 1 ? NULL : cache[i+1]->next;
+  } else {
+    // 移除表头
+    fprintf(stderr, "remove head\n");
+    head = NULL;
+  }
+
+  free(cache);
+  return head;
+}
--- algorithm/019-remove_nth_node_from_end_of_list/c/test.c
+++ algorithm/019-remove_nth_node_from_end_of_list/c/test.c
@@ -1,8 +1,45 @@
-#include "solution.h"
 #include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "solution.h"
 
-int main(int argc, char* argv[])
-{
-  // todo: unit test
+struct ListNode* list(int len, ...) {
+  struct ListNode* list = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+  struct ListNode* node = list;
+
+  va_list ap;
+  va_start(ap, len);
+  for (int i = 0; i < len; i++) {
+    node->val = va_arg(ap, int);
+    if (i + 1 == len) continue;
+    node->next = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+    node = node->next;
+  }
+  va_end(ap);
+
+  return len ? list : NULL;
+}
+
+void assertListEqual(struct ListNode* a, struct ListNode* b) {
+  while (a && b) {
+    assert(a->val == b->val);
+    a = a->next;
+    b = b->next;
+  }
+
+  assert(a == NULL && b == NULL);
+}
+
+int main(int argc, char* argv[]) {
+  // 常规测试
+  assertListEqual(removeNthFromEnd(list(5, 1, 2, 3, 4, 5), 2),
+                  list(4, 1, 2, 3, 5));
+  
+  // 删除末尾测试
+  assertListEqual(removeNthFromEnd(list(1, 1, 2), 1), list(1, 1));
+
+  // 删除头测试
+  assertListEqual(removeNthFromEnd(list(2, 1, 2), 2), list(1, 2));
   return 0;
 }
