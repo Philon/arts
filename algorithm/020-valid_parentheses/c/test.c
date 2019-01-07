785a10269b74bfcb48c0666fa8452790dd0737ca
Philon <id@philon.cn>
2019-01-07 23:04:37 +0800

    ✅ algo-20

--- algorithm/020-valid_parentheses/c/test.c
+++ algorithm/020-valid_parentheses/c/test.c
@@ -3,6 +3,13 @@
 
 int main(int argc, char* argv[])
 {
-  // todo: unit test
+  // 测试正常情况
+  assert(isValid("{[()][()()]}{}"));
+  // 测试空字符串
+  assert(isValid(""));
+  // 测试非法情况
+  assert(isValid("[{}({})}") == 0);
+  assert(isValid(")(") == 0);
+  assert(isValid("[") == 0);
   return 0;
 }
