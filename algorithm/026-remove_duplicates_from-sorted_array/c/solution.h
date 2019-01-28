8f25192b8baf02b8e92e70029bda7fe09f5ae7bc
Philon <id@philon.cn>
2019-01-28 10:53:12 +0800

    ✅ completed leetcode-26

--- algorithm/026-remove_duplicates_from-sorted_array/c/solution.h
+++ algorithm/026-remove_duplicates_from-sorted_array/c/solution.h
@@ -1,1 +1,10 @@
-// todo: algorithm implementation
+int removeDuplicates(int* nums, int numsSize) {
+  int j = 0;
+  for (int i = 1; i < numsSize; i++) {
+    if (nums[j] != nums[i]) {
+      nums[++j] = nums[i];
+    }
+  }
+
+  return numsSize > 1 ? j + 1 : numsSize;
+}
