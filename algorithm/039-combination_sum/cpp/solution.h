b519e82009f9f02fcbd0038de9603cc334b3e4d4
Philon <id@philon.cn>
2019-04-24 21:32:47 +0800

    ✅ algorithm/leetcode039

--- algorithm/039-combination_sum/cpp/solution.h
+++ algorithm/039-combination_sum/cpp/solution.h
@@ -9,17 +9,19 @@ public:
     vector<vector<int>> list;
     vector<int> combination;
     sum(candidates, target, list, combination, 0);
+
+    return list;
   }
 
 private:
   void sum(vector<int>& candidates, int target, vector<vector<int>>& list, vector<int>& combination, int begin) {
-    if (target <= 0) {
+    if (!target) {
       list.push_back(combination);
       return;
     }
 
     for (int i = begin; i != candidates.size() && target >= candidates[i]; i++) {
-      combination.push_back(i);
+      combination.push_back(candidates[i]);
       sum(candidates, target - candidates[i], list, combination, i);
       combination.pop_back();
     }
--- algorithm/039-combination_sum/cpp/test.cpp
+++ algorithm/039-combination_sum/cpp/test.cpp
@@ -1,34 +1,39 @@
 #include "solution.h"
 #include <assert.h>
 
-void assertEqual(vector<vector<int>>& combination, vector<vector<int>>& expect) {
-  assert(combination.size() == expect.size());
+void assertEqual(vector<vector<int>>& combinations, vector<vector<int>>& expect) {
+  assert(combinations.size() == expect.size());
 
-  for (auto a : combination) {
+  for (auto& a : combinations) {
     for (int i = 0; i < expect.size(); i++) {
-      auto b = expect[i];
+      auto& b = expect[i];
       if (a.size() != b.size()) {
         continue;
       }
-
+      
       for (int j = 0; j < a.size(); j++) {
         if (a[j] != b[j]) {
           goto next;
         }
       }
-      expect.erase(expect.begin() + i);
 
-next:
+      expect.erase(expect.begin() + i);
+      break;
+      
+next: continue;
     }
   }
+
+  assert(expect.size() == 0);
 }
 
 int main(int argc, char* argv[])
 {
-  vector<int> candidates = {2, 3, 5};
   Solution s;
+  vector<int> candidates = {2, 3, 5};
   auto set = s.combinationSum(candidates, 8);
   vector<vector<int>> expect = {{2, 2, 2, 2}, {2, 3, 3}, {3, 5}};
   assertEqual(set, expect);
+
   return 0;
 }
