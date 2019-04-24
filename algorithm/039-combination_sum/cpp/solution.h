0b725220cbd321117f9147d0443028a48e272cb7
Philon <id@philon.cn>
2019-04-24 08:23:28 +0800

    ✏️ update algorithm/leetcode039

--- algorithm/039-combination_sum/cpp/solution.h
+++ algorithm/039-combination_sum/cpp/solution.h
@@ -4,7 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
+  vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    sort(candidates.begin(), candidates.end());
+    vector<vector<int>> list;
+    vector<int> combination;
+    sum(candidates, target, list, combination, 0);
+  }
+
+private:
+  void sum(vector<int>& candidates, int target, vector<vector<int>>& list, vector<int>& combination, int begin) {
+    if (target <= 0) {
+      list.push_back(combination);
+      return;
     }
+
+    for (int i = begin; i != candidates.size() && target >= candidates[i]; i++) {
+      combination.push_back(i);
+      sum(candidates, target - candidates[i], list, combination, i);
+      combination.pop_back();
+    }
+  }
 };
