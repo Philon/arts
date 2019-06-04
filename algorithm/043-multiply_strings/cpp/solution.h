0b3399e217873ffe8ec33e3b089f9672f150dcdc
Philon <id@philon.cn>
2019-06-05 07:51:12 +0800

    ✏️ update leetcode-043

--- algorithm/043-multiply_strings/cpp/solution.h
+++ algorithm/043-multiply_strings/cpp/solution.h
@@ -1,10 +1,85 @@
-#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        
+  string multiply(string num1, string num2) {
+    int len1 = num1.size();
+    int len2 = num2.size();
+    vector<string> products;
+    string result = "";
+
+    reverse(num1);
+    reverse(num2);
+
+    for (int i = 0; i < len1; i++) {
+      string product = "";
+      int n1 = num1[i] - '0';
+      int carry = 0;
+
+      if (n1 < 2) {
+        products.push_back(n1 == 1 ? num2 : "0");
+        continue;
+      }
+
+      for (int j = 0; j < len2; j++) {
+        int n2 = num2[j] - '0';
+        int p = (n1 * n2) + carry;
+        product += to_string(p % 10);
+        carry = p / 10;
+      }
+
+      if (carry > 0) {
+        product += to_string(carry);
+      }
+
+      products.push_back(product);
     }
+
+    result = products[0];
+    for (int i = 1; i < products.size(); i++) {
+      if (products[i] == "0") {
+        continue;
+      }
+
+      for (int j = 0; j < i; j++) {
+        products[i] = '0' + products[i];
+      }
+      result = add(result, products[i]);
+    }
+    
+    reverse(result);
+    return result;
+  }
+
+private:
+  void reverse(string& s) {
+    int left = 0;
+    int right = s.size() - 1;
+    while (left < right) {
+      char c = s[left];
+      s[left] = s[right];
+      s[right] = c;
+      left++;
+      right--;
+    }
+  }
+
+  string add(string& a, string& b) {
+    string sum = "";
+    int carry = 0;
+    for (int i = 0; a[i] || b[i] || carry; i++) {
+
+      int n = carry;
+      if (a[i]) n += (a[i] - '0');
+      if (b[i]) n += (b[i] - '0');
+
+      sum += to_string(n % 10);
+      carry = n / 10;
+    }
+
+    return sum;
+  }
 };
--- algorithm/043-multiply_strings/cpp/test.cpp
+++ algorithm/043-multiply_strings/cpp/test.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char* argv[])
 {
   Solution s;
-  assert(s.multiply("123", "456") == "56088";
+  // assert(s.multiply("123", "456") == "56088");
+  // assert(s.multiply("1234758367", "0") == "0");
+  assert(s.multiply("234283495465340123900215", "3132340927894397834667235") == "");
   return 0;
 }
