879f1ec63ac73fc1703f31dbd1d1cb62f7a0623b
Philon <id@philon.cn>
2019-06-05 14:58:22 +0800

    ✅ leetcoce-43 beats 100%

--- algorithm/043-multiply_strings/cpp/solution.h
+++ algorithm/043-multiply_strings/cpp/solution.h
@@ -6,84 +6,32 @@ using namespace std;
 class Solution {
 public:
   string multiply(string num1, string num2) {
-    int len1 = num1.size();
-    int len2 = num2.size();
-    vector<string> products;
-    string result = "";
-
-    reverse(num1);
-    reverse(num2);
-
-    const char* a = num1.c_str();
-    const char* b = num2.c_str();
-
-    for (int i = 0; i < len1; i++) {
-      string product = "";
-      int n1 = num1[i] - '0';
-      int carry = 0;
-
-      if (n1 < 2) {
-        products.push_back(n1 == 1 ? num2 : "0");
-        continue;
-      }
-
-      for (int j = 0; j < len2; j++) {
-        int n2 = num2[j] - '0';
-        int p = (n1 * n2) + carry;
-        product += (p % 10) + '0';
-        carry = p / 10;
-      }
-
-      if (carry > 0) {
-        product += carry + '0';
-      }
-
-      products.push_back(product);
-    }
-
-    result = products[0];
-    for (int i = 1; i < products.size(); i++) {
-      if (products[i] == "0") {
-        continue;
-      }
-
-      for (int j = 0; j < i; j++) {
-        products[i] = '0' + products[i];
-      }
-      result = add(result, products[i]);
+    if (num1 == "0" || num2 == "0") {
+      return "0";
     }
     
-    reverse(result);
+    int len1 = num1.size();
+    int len2 = num2.size();
+    vector<int> products(len1 + len2, 0);
+
+    for (int i = len1-1; i >= 0; i--) {
+      int n1 = num1[i] - '0';
+      for (int j = len2-1; j >= 0; j--) {
+        int p = n1 * (num2[j] - '0');
+        p += products[i+j+1];
+        products[i+j+1] = p % 10;
+        products[i+j] += p / 10;
+      }
+    }
+
+    string result = "";
+    int len = len1 + len2;
+    int i = 0;
+    while (products[i] == 0) i++;
+    for (; i < len; i++) {
+      result += products[i] + '0';
+    }
+
     return result;
-  }
-
-private:
-  void reverse(string& s) {
-    int left = 0;
-    int right = s.size() - 1;
-    while (left < right) {
-      char c = s[left];
-      s[left] = s[right];
-      s[right] = c;
-      left++;
-      right--;
-    }
-  }
-
-  string add(string& num1, string& num2) {
-    string sum = "";
-    const char* a = num1.c_str();
-    const char* b = num2.c_str();
-    int carry = 0;
-
-    while (*a || *b || carry) {
-      int n = carry;
-      if (*a) { n += *a - '0'; a++; }
-      if (*b) { n += *b - '0'; b++; }
-      sum += (n % 10) + '0';
-      carry = n / 10;
-    }
-
-    return sum;
   }
 };
