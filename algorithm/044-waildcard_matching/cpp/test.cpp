67f543a1464afacce9547bd519bb1de55f30ac13
Philon <id@philon.cn>
2019-06-14 23:48:50 +0800

    ✏️ update leetcode-044

--- algorithm/044-waildcard_matching/cpp/test.cpp
+++ algorithm/044-waildcard_matching/cpp/test.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char* argv[])
 {
-  // todo: unit test
+  Solution s;
+  assert(!s.isMatch("b", "?*?"));
+  assert(s.isMatch("a", "a*"));
+  assert(s.isMatch("abefcdgiescdfimde", "ab*cd?i*de"));
+  assert(s.isMatch("lksdhf23ujjmsdy", "l*j?*sd?"));
   return 0;
 }
