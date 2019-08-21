78d4eccc813efe3415c85a278ee4dcd1179090f8
Philon <id@philon.cn>
2019-08-21 23:27:57 +0800

    ✅ leetcode-088

--- algorithm/088-merge_stored_array/cpp/test.cpp
+++ algorithm/088-merge_stored_array/cpp/test.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char* argv[])
 {
-  // todo: unit test
+  vector<int> nums1 = {1,2,3,0,0,0};
+  vector<int> nums2 = {2,5,6};
+  Solution s;
+  s.merge(nums1, 3, nums2, nums2.size());
+  assert(nums1 == vector<int>({1,2,2,3,5,6}));
   return 0;
 }
